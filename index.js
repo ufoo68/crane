cd66531b40ec97353c2f8e1dea3833637312ec0d
ufoo68 <mitarou.yuutarou@gmail.com>
2023-04-05 15:26:06 +0900

    package upgradeと調整

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-const Leap = require("leapjs")
-const five = require('johnny-five')
+const Leap = require("leapjs");
+const five = require("johnny-five");
 
-const motor = { right: 6, left: 5, down: 2, up: 3, forward: 8, back: 9 }
+const motor = { right: 6, left: 5, down: 2, up: 3, forward: 8, back: 9 };
 
-const board = new five.Board()
-board.on('ready',  () => {
-    const up = new five.Led(motor.up)
-    const down = new five.Led(motor.down)
-    const forward = new five.Led(motor.forward)
-    const back = new five.Led(motor.back)
-    const right = new five.Led(motor.right)
-    const left = new five.Led(motor.left)
+const board = new five.Board({ port: "COM5" });
+board.on("ready", () => {
+  const up = new five.Led(motor.up);
+  const down = new five.Led(motor.down);
+  const forward = new five.Led(motor.forward);
+  const back = new five.Led(motor.back);
+  const right = new five.Led(motor.right);
+  const left = new five.Led(motor.left);
 
-    const stop = () => {
-        back.off()
-        forward.off()
-        down.off()
-        up.off()
-        right.off()
-        left.off()
+  const stop = () => {
+    back.off();
+    forward.off();
+    down.off();
+    up.off();
+    right.off();
+    left.off();
+  };
+
+  const controller = new Leap.Controller();
+  controller.connect();
+  controller.on("hand", (hand) => {
+    console.log(hand.palmPosition);
+
+    if (hand.palmPosition[2] > 40) {
+      back.on();
     }
-
-    const controller = new Leap.Controller()
-    controller.connect()
-    controller.on('hand', hand => {
-        console.log(hand.palmPosition)
-        hand.palmPosition[0] > 0 ? right.on() : left.on()
-        hand.palmPosition[1] > 150 ? up.on() : down.on()
-        hand.palmPosition[2] < 40 ? forward.on() : back.on()
-        setTimeout(stop, 500)
-    })
-})
+    if (hand.palmPosition[2] < -20) {
+      forward.on();
+    }
+    if (hand.palmPosition[0] > 40) {
+      right.on();
+    }
+    if (hand.palmPosition[0] < -40) {
+      left.on();
+    }
+    if (hand.palmPosition[1] > 200) {
+      up.on();
+    }
+    if (hand.palmPosition[1] < 60) {
+      down.on();
+    }
+    setTimeout(stop, 500);
+  });
+});
